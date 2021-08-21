487ef9ab6b6fe8fc4cb35c5efcfffd1a250f6766
kelseyjay <87555606+kelseyjay@users.noreply.github.com>
2021-08-20 22:12:31 -0700

    changed line 104 from categories to categoryMe

--- js/script.js
+++ js/script.js
@@ -101,7 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      var chosenCategoryShortName = chooseRandomCategory (categories).short_name;
+      var chosenCategoryShortName = chooseRandomCategory (categoryMenuItems).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -131,7 +131,7 @@ function buildAndShowHomeHTML (categories) {
 // Given array of category objects, returns a random category object.
 function chooseRandomCategory (categories) {
   // Choose a random index into the array (from 0 inclusively until array length (exclusively))
-  var randomArrayIndex = Math.floor(Math.random() * categories.length);
+  var randomArrayIndex = Math.floor(Math.random( ) * categories.length);
 
   // return category object with that randomArrayIndex
   return categories[randomArrayIndex];
